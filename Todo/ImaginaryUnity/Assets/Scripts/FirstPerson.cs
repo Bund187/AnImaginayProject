bee41b6ffcaf72467547f7b74b0f13f993f0a4e9
Bund187 <gsiliutop@gmail.com>
2016-08-25 22:57:01 +0200

    Unity
    Principio prueba 2

--- Todo/ImaginaryUnity/Assets/Scripts/FirstPerson.cs
+++ Todo/ImaginaryUnity/Assets/Scripts/FirstPerson.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class FirstPerson : MonoBehaviour {
 
+    public float walkSpeed = 3;
+
     public float speedH = 2.0f;
     public float speedV = 2.0f;
     private float yaw = 0.0f;
     private float pitch = 0.0f;
-    public float walkSpeed = 3;
-    void FixedUpdate () {
+
+    void FixedUpdate()
+    {
 
         yaw += speedH * Input.GetAxis("Mouse X");
         pitch -= speedV * Input.GetAxis("Mouse Y");
