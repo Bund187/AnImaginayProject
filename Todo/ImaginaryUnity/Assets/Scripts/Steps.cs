a8a446b4e398830c7448594b940043ebc2722e9e
Bund187 <gsiliutop@gmail.com>
2016-09-05 21:43:40 +0200

    Prueba
    haciendo la prueba 1

---
+++ Todo/ImaginaryUnity/Assets/Scripts/Steps.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Steps : MonoBehaviour {
+
+    private AudioSource audioS;
+
+    void Start()
+    {
+        audioS = GetComponent<AudioSource>();
+    }
+	void Update () {
+        
+        if (transform.position.x <= -22)
+        {
+            
+            if(transform.position.z >= 1.70)
+            {
+                audioS.Stop();
+            }else
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * 5);
+            }
+        }else
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * 12);
+        }
+	}
+}
