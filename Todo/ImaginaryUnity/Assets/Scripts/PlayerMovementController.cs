3f1077d54546fa433dcde019fd450f58e90bfd37
Bund187 <gsiliutop@gmail.com>
2016-09-29 17:31:45 +0200

    Nuevo proyecto
    Cracion de la nueva prueba

---
+++ Todo/ImaginaryUnity/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerMovementController : MonoBehaviour {
+
+	float speed= 2f;
+
+	void Update() {
+		float h = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+		float v = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+		Vector3 RIGHT = transform.TransformDirection(Vector3.right);
+		Vector3 FORWARD = transform.TransformDirection(Vector3.forward);
+
+		transform.localPosition += RIGHT * h;
+		transform.localPosition += FORWARD * v;
+	}
+
+
+
+
+}
---
+++ Todo/ImaginaryUnity/Assets/Scripts/PlayerViewController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerViewController : MonoBehaviour {
+
+	public float sensitivityX = 15F;
+	public float sensitivityY = 15F;
+	public float minimumX = -360F;
+	public float maximumX = 360F;
+	public float minimumY = -60F;
+	public float maximumY = 60F;
+
+	float rotationX = 0F;
+	float rotationY = 0F;
+	Rigidbody rigid;
+	Quaternion originalRotation;
+
+	void Start ()
+	{
+		// Make the rigid body not change rotation
+		rigid=GetComponent<Rigidbody>();
+		if (rigid)
+			rigid.freezeRotation = true;
+		originalRotation = transform.localRotation;
+	}
+
+	void Update ()
+	{
+		
+		// Read the mouse input axis
+		rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+		rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+
+		rotationX = ClampAngle (rotationX, minimumX, maximumX);
+		rotationY = ClampAngle (rotationY, minimumY, maximumY);
+
+		//print ("rotacionX=" + rotationX);
+		//print ("rotacionY=" + rotationY);
+
+		Quaternion xQuaternion = Quaternion.AngleAxis (rotationX, Vector3.up);
+		Quaternion yQuaternion = Quaternion.AngleAxis (rotationY, -Vector3.right);
+
+		transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+	}
+
+	public static float ClampAngle (float angle, float min, float max)
+	{
+		if (angle < -360F)
+			angle += 360F;
+		if (angle > 360F)
+			angle -= 360F;
+		return Mathf.Clamp (angle, min, max);
+	}
+}
