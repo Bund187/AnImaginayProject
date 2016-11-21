bd1929cf482ae160ce5984d88e23a4fdc8e2a245
Bund187 <gsiliutop@gmail.com>
2016-11-21 17:25:45 +0100

    Programando
    Depthmanager y empezando con los dialogos

---
+++ Todo/ImagineUnity/Assets/Scripts/Depthmanager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Depthmanager : MonoBehaviour {
+
+
+	SpriteRenderer spriteRend;
+	int order;
+
+	void Start () {
+		spriteRend = GetComponent<SpriteRenderer> ();
+		order = spriteRend.sortingOrder;
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.tag == "Player") {
+			spriteRend.sortingOrder = col.gameObject.GetComponent<SpriteRenderer> ().sortingOrder + 1;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		if (col.tag == "Player") {
+			spriteRend.sortingOrder = order;
+		}
+	}
+}
---
+++ Todo/ImagineUnity/Assets/Scripts/DialogueController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+
+public class DialogueController : MonoBehaviour {
+
+	ArrayList dialogue=new ArrayList();
+	string idiom="esp";
+	int i=0;
+	bool isPress;
+
+	void OnCollisionStay2D(Collision2D col){
+		if (col.collider.tag == "Player") {
+
+			StreamReader reader = new StreamReader (Application.dataPath + "/StreamingAssets" + "/" + gameObject.name + "/" + idiom + "Dialogue.txt");
+			while (!reader.EndOfStream) {
+				dialogue.Add (reader.ReadLine ());
+			}
+
+			if ((Input.GetAxisRaw ("Fire1") != 0)) {
+				if (!isPress) {
+					print ("linea 1 = " + dialogue [i]);
+					i++;
+					isPress = true;
+				}
+			} else
+				isPress = false;
+		}
+	}
+
+	public string Idiom {
+		get {
+			return this.idiom;
+		}
+		set {
+			idiom = value;
+		}
+	}
+}
